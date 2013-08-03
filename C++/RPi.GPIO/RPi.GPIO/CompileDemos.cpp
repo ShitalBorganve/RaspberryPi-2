9b860525a900a2b7600d984b839926bb6a17a7ba
jamolnng <guitarerman@gmail.com>
2013-08-02 20:58:21 -0500

    Update

--- C++/RPi.GPIO/RPi.GPIO/CompileDemos.cpp
+++ C++/RPi.GPIO/RPi.GPIO/CompileDemos.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
 	cout << "Compiling LED_Demo" << endl;
-	system("sudo g++ LED_Demo.cpp GPIO.h GPIO.cpp Utils.h Utils.cpp LCD.h LCD.cpp -o LED_Demo");
+	system("sudo g++ LED_Demo.cpp GPIO.h GPIO.cpp Utils.h Utils.cpp -o LED_Demo");
 	cout << "Finished compiling LED_Demo" << endl;
 	cout << "Compiling LCD_Demo" << endl;
 	system("sudo g++ LCD_Demo.cpp GPIO.h GPIO.cpp Utils.h Utils.cpp LCD.h LCD.cpp -o LCD_Demo");
